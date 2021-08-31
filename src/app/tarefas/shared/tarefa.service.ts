4e90a1b65c1ba894eccaf82af292ece49467ccd0
Silva <smfssilva@yahoo.com>
2021-08-31 14:14:03 -0300

    Criando o serviço de gerenciamento de tarefas 1/2

---
+++ src/app/tarefas/shared/tarefa.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+import { Tarefa } from '.';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TarefaService {
+
+  constructor() { }
+
+  listarTodos() : Tarefa[] {
+    const tarefas = localStorage['tarefas'];
+    return tarefas ? JSON.parse(tarefas) : [];
+  }
+
+  cadastrar(tarefa: Tarefa) : void {
+    const tarefas = this.listarTodos();
+    tarefa.id = new Date().getTime();
+    tarefas.push(tarefa);
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+}
