50c2ff61b543ac497f5a76f8178356b99fbbbd15
Silva <smfssilva@yahoo.com>
2021-08-31 16:14:06 -0300

    Registrando as rotas de cadastro de tarefas

--- src/app/tarefas/tarefas-routing.module.ts
+++ src/app/tarefas/tarefas-routing.module.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 
 import { ListarTarefasComponent } from "./listar";
+import { CadastrarTarefasComponent } from "./cadastrar";
 
 export const TarefaRoutes: Routes = [
   {
@@ -10,5 +11,9 @@ export const TarefaRoutes: Routes = [
   {
     path: 'tarefas/listar',
     component: ListarTarefasComponent
+  },
+  {
+    path: 'tarefas/cadastrar',
+    component: CadastrarTarefasComponent
   },
 ];
