bf9dc479abee3bc83dfbf60fbd9fd103c199dbf4
Silva <smfssilva@yahoo.com>
2021-09-01 16:01:54 -0300

    Implementando a edição de tarefas

--- src/app/tarefas/cadastrar/cadastrar-tarefas.component.ts
+++ src/app/tarefas/cadastrar/cadastrar-tarefas.component.ts
@@ -11,7 +11,7 @@ import { NgForm } from '@angular/forms';
 })
 export class CadastrarTarefasComponent implements OnInit {
 
-  @ViewChild('formTarefa') formTarefa: NgForm;
+  @ViewChild('formTarefa', {static: true}) formTarefa: NgForm;
   tarefa: Tarefa;
 
   constructor(
